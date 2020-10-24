d6f517d7b7f40e4e9cf8a3f5db3c981c88cdf0eb
Maciej Krüger <mkg20001@gmail.com>
2020-10-24 19:44:16 +0200

    feat: better hooking api

--- src/index.js
+++ src/index.js
@@ -13,69 +13,69 @@ const Relish = require('relish')({
   messages: {},
 })
 
-const init = async ({appName, frontend, celariumModel, celariumConfig}, config) => {
-  const log = pino({name: appName})
+const init = ({
+  appName, frontend, celariumModel, dbBackend,
+  onStartup, onShutdown,
+}) =>
+  async config => {
+    const log = pino({name: appName})
 
-  // const mongodbDB = config.db.url.split('/').pop().split('?').shift() // get uppercase part: mongodb://url:port/DB?something
-
-  config.api.extraConfig = {
-    /* cache: {
-      provider: {
-        constructor: CatboxMongoDB,
-        options: {
-          uri: config.mongodb,
-          partition: mongodbDB,
+    config.api.extraConfig = {
+      routes: {
+        validate: {
+          failAction: Relish.failAction,
         },
       },
-    }, */
-    routes: {
-      validate: {
-        failAction: Relish.failAction,
-      },
-    },
-  }
+    }
 
-  config.api.getUser = () => 0 // TODO: add auth
+    config.api.getUser = () => 0 // TODO: add auth
 
-  const {
-    start,
-    stop,
-    DBM,
-    API,
-  } = await celarium(celariumModel, celariumConfig, {db: config.db, api: config.api})
+    const {
+      start,
+      stop,
+      DBM,
+      API,
+    } = await celarium(celariumModel, {api: 'hapi', db: dbBackend, beautify: false}, {db: config.db, api: config.api})
 
-  const server = API._hapi
+    const server = API._hapi
 
-  await server.register({
-    plugin: require('hapi-pino'),
-    options: {name: appName},
-  })
-
-  if (global.SENTRY) {
     await server.register({
-      plugin: require('hapi-sentry'),
-      options: {client: global.SENTRY},
+      plugin: require('hapi-pino'),
+      options: {name: appName},
     })
+
+    if (global.SENTRY) {
+      await server.register({
+        plugin: require('hapi-sentry'),
+        options: {client: global.SENTRY},
+      })
+    }
+
+    await server.register({
+      plugin: require('@hapi/inert'),
+    })
+
+    if (frontend) {
+      require('hapi-spa-serve')(server, {assets: frontend})
+    }
+
+    if (onStartup) {
+      await onStartup(log, server, config, DBM, API)
+    }
+
+    await start()
+
+    async function shutdown() {
+      if (onShutdown) {
+        await onShutdown()
+      }
+      stop()
+    }
+
+    process.on('SIGINT', shutdown)
+
+    process.on('SIGTERM', shutdown)
   }
-
-  await server.register({
-    plugin: require('@hapi/inert'),
-  })
-
-  if (frontend) {
-    require('hapi-spa-serve')(server, {assets: frontend})
-  }
-
-  await start()
-
-  process.on('SIGINT', () => {
-    stop()
-  })
-
-  process.on('SIGTERM', () => {
-    stop()
-  })
-}
 
 module.exports = init
 
