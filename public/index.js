12add8561efc0438be52fc813e960c579eb47d3c
Maciej Krüger <mkg20001@gmail.com>
2020-11-08 16:57:38 +0100

    feat: shared locales

--- public/index.js
+++ public/index.js
@@ -26,6 +26,44 @@ if (!window.fetch) {
 require('sweetalert2')
 window.swal = require('sweetalert2')
 
+/**
+ * Simple object check.
+ *
+ * @param item
+ * @returns {boolean}
+ */
+export function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item))
+}
+
+/**
+ * Deep merge two objects.
+ *
+ * @param target
+ * @param ...sources
+ * @param {...any} sources
+ */
+export function mergeDeep(target, ...sources) {
+  if (!sources.length) return target
+  const source = sources.shift()
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, {[key]: {}})
+        mergeDeep(target[key], source[key])
+      } else {
+        Object.assign(target, {[key]: source[key]})
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources)
+}
+
+// Code copied from linked Stack Overflow question
+// https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge
+
 export default ({
   messages,
   api,
@@ -39,6 +77,8 @@ export default ({
       integrations: [new Sentry.Integrations.Vue({Vue})],
     })
   }
+
+  messages = mergeDeep(require('./locales'), messages)
 
   Vue.use(VueResource)
   Vue.use(GlobalOptions, ['api', 'config', 'user', 'ui'])
